f25e17a12a2ff188f75bf7d2e3df9ccc897ec7c4
deujarohan <rrohandeuja@gmail.com>
2024-05-30 20:17:40 +0545

    just starting with project

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const userRoute = require("./src/routes/userRoutes");
 const app = express();
 
 const port = process.env.PORT || 8000;
+
+//Routes
+app.use("/", userRoute);
 
 app.listen(port, () => {
   console.log(`Application is running in ${port} port.`);
