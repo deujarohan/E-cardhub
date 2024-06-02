66541d2ee05eacf1f6168beea945ca1be2dbe34f
deujarohan <rrohandeuja@gmail.com>
2024-06-02 13:42:27 +0545

    setting up user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controller/authController");
 
 router.get("/", (req, res) => {
   res.send("Welcom to my first project");
 });
 
-router.get("/login", (req, res) => {
-  res.send("Hi i am user");
-});
+router.post("/register", authController.register);
 
-router.get("/register", (req, res) => {
-  res.send("Creating an user account");
-});
+router.get("/login", authController.login);
 
 router.post("/register", (req, res) => {
   console.log(req.body);
